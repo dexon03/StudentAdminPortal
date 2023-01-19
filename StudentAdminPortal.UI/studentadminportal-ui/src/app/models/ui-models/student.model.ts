c49c98da02f0f8e4f8ab32bb4b8b77014b06067e
dexon03 <dima0958757803@gmail.com>
2023-01-19 13:56:32 +0200

    front init

---
+++ StudentAdminPortal.UI/studentadminportal-ui/src/app/models/ui-models/student.model.ts
@@ -0,0 +1,17 @@
+import { Address } from "./address.model";
+import { Gender } from "./gender.model";
+
+
+
+export interface Student{
+  id:String,
+  firstName:String,
+  lastName:String,
+  dateOfBirth: String,
+  email : String,
+  phoneNumber : Number,
+  profileImageUrl : String,
+  genderId : String,
+  gender : Gender,
+  address : Address,
+}
